ba0998ab4ad79dd2a76b494a7e14e083952355dd
ge56sil <francesco.vaccaro@tum.de>
2022-07-14 15:31:42 +0200

    Answer problem type "what is 18 plus 12" v5

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,25 +13,33 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
+        } else if (query.contains("what is")) { // TODO extend the programm here
 
             // Beispiel: what is 18 plus 12
+//
+//            String shortenedQuery = query.replaceAll("[^0-9]", "");
+//
+//            int summand1 = Integer.parseInt(query.substring(0,2));
+//            int summand2 = Integer.parseInt(query.substring(2,4));
 
 
-            int summand1 = Integer.parseInt(query.substring(8,10));
-            int summand2 = Integer.parseInt(query.substring(16,18));
 
 //            String[] stringArray = query.split(" ");
 //            int summand1 = Integer.parseInt(stringArray[2]);
 //            int summand2 = Integer.parseInt(stringArray[4]);
 //
-//
-//
-            int intResult = summand1 + summand2;
-            String stringResult = String.valueOf(intResult);
-            return stringResult;
 
-//            return "";
+            int summand1 = Integer.parseInt(query.substring(8,10));
+            int summand2 = Integer.parseInt(query.substring(16,18));
+
+            int intResult = summand1 + summand2;
+
+            return String.valueOf(intResult);
+
+        }
+
+        else {
+            return "";
         }
     }
 }
