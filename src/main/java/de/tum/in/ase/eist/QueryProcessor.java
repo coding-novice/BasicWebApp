5c4c994ca9ec03a08bab81a0f2f90cd7c97767d8
ge56sil <francesco.vaccaro@tum.de>
2022-07-14 15:16:05 +0200

    Answer problem type "what is 18 plus 12" v3

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -18,15 +18,17 @@ public class QueryProcessor {
             // Beispiel: what is 18 plus 12
 
 
-            String[] stringArray = query.split(" ");
-            int summand1 = Integer.parseInt(stringArray[2]);
-            int summand2 = Integer.parseInt(stringArray[4]);
+//            String[] stringArray = query.split(" ");
+//            int summand1 = Integer.parseInt(stringArray[2]);
+//            int summand2 = Integer.parseInt(stringArray[4]);
+//
+//
+//
+//            int intResult = summand1 + summand2;
+//            String stringResult = String.valueOf(intResult);
+//            return stringResult;
 
-
-
-            int intResult = summand1 + summand2;
-            String stringResult = String.valueOf(intResult);
-            return stringResult;
+            return "";
         }
     }
 }
